f6bf90a7631189129978218ea442a08a58e5967a
potion <potionkr@gmail.com>
2019-12-14 14:49:53 +0900

    generated error fix

--- src/generated/MiniCParser.java
+++ src/generated/MiniCParser.java
@@ -1,4 +1,4 @@
-// Generated from D:/Project/Intellij-workspace/C2JavaByteCode/src\MiniC.g4 by ANTLR 4.7.2
+// Generated from /Users/jongun/cnu/IntelliJ_Project/C2JavaByteCode/src/MiniC.g4 by ANTLR 4.7.2
  
 package generated;
 
